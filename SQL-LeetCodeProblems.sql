fc45b82ea53424949d76140f74e2ab57a1294a0a
Rochak Wadhwa <rochak.wadhwa@gmail.com>
2025-07-21 10:53:56 +0530

    Update SQL-LeetCodeProblems.sql

--- SQL-LeetCodeProblems.sql
+++ SQL-LeetCodeProblems.sql
@@ -1,1 +1,7 @@
 ==============================================================================
+181. Employees Earning More Than Their Managers
+
+select a.name as Employee from Employee a, Employee b
+where a.managerId = b.id
+and a.salary > b.salary;
+==============================================================================
