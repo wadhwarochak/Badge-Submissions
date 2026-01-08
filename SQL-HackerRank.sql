1bd85cc741a23e24ad2cd71d71e713785246693d
wadhwarochak <rochak.wadhwa@gmail.com>
2026-01-08 16:17:53 +0530

    Focusability - 08

--- SQL-HackerRank.sql
+++ SQL-HackerRank.sql
@@ -1,33 +1,3 @@
 ===============================================================
-Revising the Select Query I
-
-SELECT * FROM CITY where population > 100000 and CountryCode  = 'USA';
 ===============================================================
-Revising the Select Query II
-
-SELECT NAME FROM CITY where population > 120000 and CountryCode = 'USA';
-===============================================================
-SELECT ALL
-
-SELECT * FROM CITY;
-===============================================================
-SELECT using ID
-
-SELECT * FROM CITY where ID = 1661;
-===============================================================
-Japanese Cities Attributes
-
-SELECT * FROM CITY where COUNTRYCODE = 'JPN';
-===============================================================
-Japanese City Names
-
-SELECT NAME FROM CITY where COUNTRYCODE = 'JPN';
-===============================================================
-Weather Observation I
-
-SELECT CITY, STATE FROM STATION;
-===============================================================
-Weather Observation III
-
-SELECT DISTINCT CITY FROM STATION where ID%2=0;
 ===============================================================
