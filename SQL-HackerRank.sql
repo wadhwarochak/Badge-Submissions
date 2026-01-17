7bf6067fdf03475c08a23e44ea3b29ce4352ac95
wadhwarochak <rochak.wadhwa@gmail.com>
2026-01-17 07:21:18 +0530

    Adding SQL Solutions

--- SQL-HackerRank.sql
+++ SQL-HackerRank.sql
@@ -3,3 +3,8 @@ Revising the Select Query I
 
 SELECT * FROM CITY WHERE population > 100000 AND CountryCode = 'USA';
 ===============================================================
+Revising the Select Query II
+
+SELECT NAME FROM CITY WHERE 
+population >120000 and CountryCode = 'USA';
+===============================================================
--- SQL-LeetCode - Select.sql
+++ SQL-LeetCode - Select.sql
@@ -3,6 +3,13 @@
 
 SELECT product_id FROM Products WHERE low_fats = 'Y'and recyclable = 'Y';
 ===============================================================
+584. Find Customer Referee
+
+SELECT name FROM Customer
+WHERE referee_id is NULL or referee_id != 2
+
+SELECT name FROM Customer
+WHERE IFNULL(referee_id,0) <>2
 ===============================================================
 ===============================================================
 ===============================================================
