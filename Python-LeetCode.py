6f29781f0c553d1c1fe053d5ee7772906317da81
wadhwarochak <rochak.wadhwa@gmail.com>
2026-01-18 07:16:20 +0530

    Momentum - 18

--- Python-LeetCode.py
+++ Python-LeetCode.py
@@ -93,3 +93,11 @@ class Solution:
             result=result+s[indices.index(i)]
         return result
 ===============================================================
+231. Power of Two
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        while(n>0 and n%2==0):
+            n=n/2
+        return n==1
+===============================================================
